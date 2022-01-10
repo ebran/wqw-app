49feb760975ab88e68c474cf49385f834fbfae95
Erik G. Brandt <erik.brandt@shaarpec.com>
2022-01-10 23:01:07 +0100

    ✨ Basic tasks

--- src/wqw_app/app.py
+++ src/wqw_app/app.py
@@ -1,11 +1,28 @@
 """
 Web app
 """
-
+from dataclasses import asdict
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+# from devtools import debug
+
+from wqw_app.worker import backend, fibonacci
+
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    """Startup logic goes here."""
+    await backend.init()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    """Shutdown logic goes here."""
+    await backend.close()
 
 
 @app.get("/")
@@ -14,7 +31,36 @@ def read_root() -> JSONResponse:
     return JSONResponse(content={"Hello": "World"}, status_code=200)
 
 
-@app.get("/tasks/{task_id}")
-def read_item(task_id: int) -> JSONResponse:
-    """Get task."""
+@app.post("/fib")
+async def post_task(number: int) -> JSONResponse:
+    """Post a calculation task."""
+
+    if job := await backend.redis.enqueue_job(fibonacci.__name__, number):
+        if job_info := await job.info():
+            return JSONResponse(
+                content={
+                    "task_id": job.job_id,
+                    "submitted_at": job_info.enqueue_time.strftime("%c"),
+                },
+                status_code=200,
+            )
+
+    return JSONResponse(content={"error": "Failed to enqueue task."}, status_code=500)
+
+
+@app.get("/fib")
+async def read_task_list() -> JSONResponse:
+    """Get list of calculation tasks."""
+    task_list = []
+    for task in await backend.redis.queued_jobs():
+        task_dict = asdict(task)
+        task_dict["submitted_at"] = task_dict.pop("enqueue_time").strftime("%c")
+        task_list.append(task_dict)
+
+    return JSONResponse(content=task_list, status_code=200)
+
+
+@app.get("/fib/{task_id}")
+def read_task(task_id: int) -> JSONResponse:
+    """Get status for a calculation task."""
     return JSONResponse(content={"task_id": task_id}, status_code=200)
--- src/wqw_app/worker.py
+++ src/wqw_app/worker.py
@@ -1,6 +1,52 @@
 """
 Worker.
 """
+import asyncio
+from typing import Optional, TypedDict
+from concurrent import futures
+
+from arq.connections import create_pool, RedisSettings, ArqRedis
+
+from devtools import debug
+
+
+class WorkerContext(TypedDict):
+    """Context for workers."""
+
+    job_id: str
+
+
+# async def startup(ctx):
+#     """Startup logic goes here."""
+#     pass
+
+
+# async def shutdown(ctx):
+#     """Shutdown logic goes here."""
+#     pass
+
+
+class Backend:
+    """arq backend."""
+
+    def __init__(self):
+        self._redis: Optional[ArqRedis] = None
+
+    async def init(self):
+        """Initialize a connection pool to the backend."""
+        self._redis = await create_pool(RedisSettings())
+
+    async def close(self):
+        """Close the connection pool to the backend."""
+        self.redis.close()
+        await self.redis.wait_closed()
+
+    @property
+    def redis(self):
+        """Return the redis connection."""
+        if self._redis is None:
+            raise RuntimeError("Fatal: No redis connection.")
+        return self._redis
 
 
 def fib(number: int) -> int:
@@ -14,6 +60,32 @@ def fib(number: int) -> int:
     """
     assert number > 0
 
-    if number in (1, 2):
+    if number < 3:
         return 1
     return fib(number - 1) + fib(number - 2)
+
+
+# pylint: disable=unused-argument
+async def fibonacci(ctx: Optional[WorkerContext], number: int) -> int:
+    """Async wrapper around fib function."""
+    loop = asyncio.get_running_loop()
+
+    with futures.ProcessPoolExecutor() as pool:
+        print(f"Context is {ctx}")
+        result = await loop.run_in_executor(pool, fib, number)
+
+    return result
+
+
+# WorkerSettings defines the settings to use when creating the work,
+# it's used by the arq cli
+# pylint: disable=too-few-public-methods
+class WorkerSettings:
+    """Settings for the worker."""
+
+    functions = [fibonacci]
+    # on_startup = startup
+    # on_shutdown = shutdown
+
+
+backend = Backend()
